423912287385e9f13d426608f197631aeafb2c06
Nagitu <denipraa1@gmail.com>
2024-08-20 06:42:06 +0700

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send.json('Hello, World! This is your Express app running on Vercel!');
+  res.json('Hello, World! This is your Express app running on Vercel!');
 });
 
 // Export the Express app as a module
